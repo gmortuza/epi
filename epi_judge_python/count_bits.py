fc157c51b5384f2d7022b547a816666599cb7dde
Golam Mortuza <golammortuza@eng402048.boisestate.edu>
2019-09-13 12:55:41 -0600

    count bit done

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -3,7 +3,11 @@ from test_framework import generic_test
 
 def count_bits(x):
     # TODO - you fill in here.
-    return 0
+    result = 0
+    while x:
+        result = result + (x & 1)
+        x = x >> 1
+    return result
 
 
 if __name__ == '__main__':
