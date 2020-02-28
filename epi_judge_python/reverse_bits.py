aa3e9473354f60448bea25c89121a8a087b7b092
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-02-27 20:39:19 -0700

    Finished the reverse bits

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -2,10 +2,13 @@ from test_framework import generic_test
 
 
 def reverse_bits(x):
-    # TODO - you fill in here.
-    return 0
+    # Counting the bits
+    for i in range(32):
+        if (x >> i) & 1 != (x >> (63 - i)) & 1:
+            x ^= (1 << i) | (1 << (63-i))
+    return x
 
-
+ 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("reverse_bits.py", "reverse_bits.tsv",
--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -7,7 +7,9 @@ def swap_bits(x, i, j):
         x ^= 1 << j
     return x
 
+
 if __name__ == '__main__':
+
     exit(
         generic_test.generic_test_main("swap_bits.py", 'swap_bits.tsv',
                                        swap_bits))
