199722fe0727eef7f7e764170dfb59ecf1fca7db
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-05-18 23:50:15 -0600

    is_number_palindromic.py done

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def is_palindrome_number(x):
-    # TODO - you fill in here.
-    return True
+    if x <= 0:
+        return x == 0
+    reverse_digt = 0
+    x_for_rev = x
+    while x_for_rev:
+        reverse_digt = reverse_digt * 10 + x_for_rev % 10
+        x_for_rev //= 10
+    return x == reverse_digt
 
 
 if __name__ == '__main__':
