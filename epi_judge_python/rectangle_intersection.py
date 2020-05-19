4c1ed458248abee6d1cb374c480fcda5e13383da
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-05-19 01:04:39 -0600

    rectangle_intersection.py done

--- epi_judge_python/rectangle_intersection.py
+++ epi_judge_python/rectangle_intersection.py
@@ -7,8 +7,10 @@ Rectangle = collections.namedtuple('Rectangle', ('x', 'y', 'width', 'height'))
 
 
 def intersect_rectangle(R1, R2):
-    # TODO - you fill in here.
-    return Rectangle(0, 0, 0, 0)
+    # Checking if they intersect
+    if not ( R1.x <= R2.x + R2.width and R1.x + R1.width >= R2.x and R1.y <= R2.y + R2.height and R1.y + R1.height >= R2.y):
+        return Rectangle(0, 0, -1, -1)
+    return Rectangle(max(R1.x, R2.x), max(R1.y, R2.y), min(R1.x + R1.width, R2.x + R2.width) - max(R1.x, R2.x), min(R1.y + R1.height, R2.y + R2.height) - max(R1.y, R2.y))
 
 
 def intersect_rectangle_wrapper(R1, R2):
