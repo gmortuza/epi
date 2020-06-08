86d9955fee6a2cc14b09665d1d04387b0ee2dcc6
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-06-08 13:18:48 -0600

    Buy sell stock done

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,8 +2,12 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices):
-    # TODO - you fill in here.
-    return 0.0
+    minimum_price, profit = float('inf'), 0
+    for price in prices:
+        max_profit_sell_today = price - minimum_price
+        profit = max(max_profit_sell_today, profit)
+        minimum_price = min(price, minimum_price)
+    return profit
 
 
 if __name__ == '__main__':
